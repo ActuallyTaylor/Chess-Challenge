19ecc6fc0fba604a43fe9862e7c1521207bd40dc
ActuallyTaylor <zachary.lineman@gmail.com>
2023-07-27 16:19:58 -0400

    Added move ordering

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,6 +1,10 @@
 ﻿// I have not written C# in any capacity in years. I have also barely ever
 // played chess. So this is a mess
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using ChessChallenge.API;
 
 public class MyBot : IChessBot {
@@ -10,17 +14,18 @@ public class MyBot : IChessBot {
         // Negamax Root
         int bestEval = int.MinValue;
 
-        Move[] moves = board.GetLegalMoves();
+        Move[] moves = getOrderedMoves(board);
         Move bestMove = moves[0];
 
         foreach( Move move in moves ) {
             board.MakeMove(move);
-            int eval = -negamax(board, int.MinValue, int.MaxValue, 3);
+            int eval = -negamax(board, int.MinValue, int.MaxValue, 5);
+            board.UndoMove(move);
+
             if( eval >= bestEval ) {
                 bestEval = eval;
                 bestMove = move;
             }
-            board.UndoMove(move);
         }
         // End Negamax Root
 
@@ -28,11 +33,13 @@ public class MyBot : IChessBot {
     }
 
     private int negamax(Board board, int alpha, int beta, int depth) {
-        if( depth == 0 ) {
+        if( depth == 0 || board.IsDraw() || board.IsInCheck() ||
+            board.IsInCheckmate() || board.IsRepeatedPosition() ||
+            board.IsInsufficientMaterial() ) {
             return evaluate(board);
         }
 
-        foreach( Move move in board.GetLegalMoves() ) {
+        foreach( Move move in getOrderedMoves(board)) {
             board.MakeMove(move);
             int score = -negamax(board, -beta, -alpha, depth - 1);
             board.UndoMove(move);
@@ -46,6 +53,70 @@ public class MyBot : IChessBot {
         }
 
         return alpha;
+    }
+
+    private int quiesceSearch(Board board, int alpha, int beta) {
+        int eval = evaluate(board);
+        if ( eval >= beta) {
+            return beta;
+        }
+        if (alpha < eval ) {
+            alpha = eval;
+        }
+
+        Move[] captures = getOrderedMoves(board, true);
+        foreach(Move capture in captures) {
+            board.MakeMove(capture);
+            int score = -quiesceSearch(board, -beta, -alpha);
+            board.UndoMove(capture);
+
+            if (score >= beta) {
+                return beta;
+            }
+            if (score > alpha) {
+                alpha = score;
+            }
+        }
+
+        return alpha;
+    }
+
+    private Move[] getOrderedMoves(Board board, bool capturesOnly = false) {
+        Move[] moves = board.GetLegalMoves(capturesOnly);
+        int[] moveScores = new int[moves.Length];
+
+        for( int i = 0; i < moves.Length; i++ ) {
+            Move move = moves[i];
+            int moveScore = 0;
+            Piece movingPiece = board.GetPiece(move.StartSquare);
+            Piece capturePiece = board.GetPiece(move.TargetSquare);
+
+            // Prioritise captures based on the value of the captured piece.
+            if( move.IsCapture && !capturePiece.IsNull) {
+                moveScore += 10 * pieceValues[(int) capturePiece.PieceType];
+            }
+            // Prioritise moving to a promotion
+            if( move.IsPromotion) {
+                moveScore += pieceValues[( int ) move.PromotionPieceType];
+            }
+            //// Penalize moving into sqaures that are attacked by the opponent.
+            if( board.SquareIsAttackedByOpponent(move.TargetSquare) ) {
+                moveScore -= pieceValues[(int) movingPiece.PieceType];
+            }
+            //// Prioritise moving out of the way of attacks
+            if( board.SquareIsAttackedByOpponent(move.StartSquare) ) {
+                moveScore += pieceValues[(int) movingPiece.PieceType];
+            }
+
+            moveScores[i] = moveScore;
+        }
+
+        // Sort the moves based on the scores in moveScores
+        Array.Sort(moveScores, moves);
+        // Reverse the list so the biggest scores come first.
+        Array.Reverse(moves);
+
+        return moves;
     }
 
     // Evaluate the board and return a value based on the current pieces in play.
@@ -65,7 +136,9 @@ public class MyBot : IChessBot {
         foreach( PieceList pieceList in board.GetAllPieceLists() ) {
             foreach( Piece piece in pieceList ) {
                 if( piece.IsWhite != isWhite ) {
-                    continue;
+                    // Evaluate if the enemy is attacking any of our pieces
+                    
+                    continue; // Skip. Don't add the general points
                 }
 
                 value += pieceValues[( int ) piece.PieceType];
@@ -75,3 +148,4 @@ public class MyBot : IChessBot {
         return value;
     }
 }
+
